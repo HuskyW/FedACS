f57a6c447880960d5197bd3673509b6006125005
WangZibo <wangzibo@sjtu.edu.cn>
2021-01-15 21:32:40 +0800

    adjust param

--- prun.sh
+++ prun.sh
@@ -3,6 +3,7 @@
 rm -f ./save/acc/*
 rm -f ./save/domi/*
 rm -f ./save/reward/*
+rm -f ./save/hitmap/*
 rm -f ./templog/*
 
 time1=$(date)
@@ -12,7 +13,7 @@ for trial in {1..5..1}
 do
 {
 echo "Trial $trial"
-python main_fed.py --epochs 1000 --local_ep 5 --local_bs 50 --iid 4 --testing 5  --client_sel 0 --num_data 250 --extension 8 --cut_thres 0 --log_idx $trial #&> ./templog/$trial.log
+python main_fed.py --epochs 1000 --local_ep 5 --local_bs 400 --iid 2 --testing 5  --client_sel 1 --num_data 2000 --extension 8 --historical_rounds 5 --log_idx $trial #&> ./templog/$trial.log
 }&
 
 rem=$(($trial%5))
